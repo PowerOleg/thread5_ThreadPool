b13aba70864b76b75c4682a8e4a27157c6662667
PowerOleg <geraskin.oleg@mail.ru>
2024-10-06 18:43:30 +0300

    done with problems

--- ThreadPool/main.cpp
+++ ThreadPool/main.cpp
@@ -9,57 +9,75 @@
 #include <exception>
 #include <stdexcept>
 
+std::mutex m;
 
 template <class F>
 class Safe_queue
 {
-public:
-	Safe_queue()
-	{};
+	using Task = std::packaged_task<void()>;
 
-	Safe_queue(const Safe_queue& safeQueue) : tasks_(safeQueue.tasks_), future_queue_{safeQueue.future_queue_}
+public:
+	Safe_queue() {};
+
+	Safe_queue(Safe_queue&& safeQueue) : tasks_(std::move(safeQueue.tasks_)), future_queue_{ std::move(safeQueue.future_queue_) } 
 	{
-		if (&safe_queue_ == this)
+		if (&safeQueue == this)
 		{
 			throw std::invalid_argument("Invalid_argument: this object and the argument object are the same");
 		}
 	}
 
-
-	//template <class F>
-	/*std::future<void>*/void Submit(F && f)
+	void Submit(F && f)
 	{
 		std::future<void> fut;
 
-
 		{
-			std::unique_lock<std::mutex> lock(m_);
+			std::unique_lock<std::mutex> lock(m);//m_);								//mutex???
 			fut = tasks_.emplace(std::forward<F>(f)).get_future();
-			future_queue_.emplace(fut);
+			future_queue_.emplace(std::move(fut));											
 		}
 		cv_.notify_one();
-		//return fut;
-		
+	}
+
+	void start_task()
+	{
+		Task task = std::move(this->tasks_.front());
+		this->tasks_.pop();
+		if (task.valid())
+		{
+			task();
+		}
+	}
+
+	bool has_task()
+	{
+		return !this->tasks_.empty();
 	}
 
 	void get_result()
 	{
-		future_queue_.front().get();
+		if (!this->future_queue_.empty())
+		{
+			auto future = std::move(this->future_queue_.front());
+			this->future_queue_.pop();
+			future.get();
+		}
+		
+	
 	}
 
 private:
 	std::queue<std::future<void>> future_queue_;
 	std::queue<Task> tasks_;
-	std::mutex m_;
+	//std::mutex m_;
 	std::condition_variable cv_;
 };
 
+
 class ThreadPool
 {
-	using Task = std::packaged_task<void()>;
-
 public:
-	ThreadPool(size_t threads_count, Safe_queue<std::function<void()>> safe_queue_) : safe_queue{ safe_queue_ }
+	ThreadPool(size_t threads_count, Safe_queue<std::function<void()>>&& safe_queue_) : safe_queue{ std::move(safe_queue_) }
 	{
 		threads_.reserve(threads_count);
 		for (size_t i = 0; i < threads_count; i++)
@@ -72,20 +90,6 @@ public:
 	{
 		join();
 	}
-
-	//template <class F>//template <typename F>
-	//std::future<void> Submit(F&& f)
-	//{
-	//	std::future<void> fut;
-
-
-	//	{
-	//		std::unique_lock<std::mutex> lock(m_);
-	//		fut = tasks_.emplace(std::forward<F>(f)).get_future();
-	//	}
-	//	cv_.notify_one();
-	//	return fut;
-	//}
 
 	void shutdown()
 	{
@@ -108,37 +112,27 @@ private:
 	}
 
 private:
-	void work()//loop()//here is the main idea of pool of tasks
+	void work()
 	{
 		while (true)
 		{
-			Task task;
-
-
-
+			
 			{
 				std::unique_lock<std::mutex> lock(m);
 				cv.wait(lock, [this] {
-					return !tasks_.empty() || shutdown_;
+					return safe_queue.has_task() || shutdown_;
 					});
 
-				if (!tasks_.empty())
+				if (safe_queue.has_task())
 				{
-					task = std::move(tasks_.front());
-					tasks_.pop();
+					safe_queue.start_task();
 				}
 			}
-
-			if (task.valid())
-			{
-				task();
-			}
-
 
 			if (shutdown_)
 			{
 			std::unique_lock<std::mutex> lock(m);
-				if (tasks_.empty())
+				if (!safe_queue.has_task())
 				{
 					break;
 				}
@@ -149,37 +143,34 @@ private:
 	private:
 		std::vector<std::thread> threads_;
 		Safe_queue<std::function<void()>> safe_queue;
-		//std::queue<Task> tasks_;
-
-		std::mutex m;
+		//std::mutex m;
 		std::condition_variable cv;
 		bool shutdown_ = false;
 };
 
 
-
-//сделать: оператор копирования/перемезщения, конструктор перемещения у queue
-int main(int argc, char** argv)//we have 4 threads what stands for number of computer cores and tasks in 
+//problems: 1)need to move mutex from global parameter, 2) why future start_task without .get()?
+int main(int argc, char** argv)
 {
 	unsigned int cores_quantity = std::thread::hardware_concurrency();
-	Safe_queue<std::function<void()>/*std::packaged_task<void()>*/> safe_queue;
-	ThreadPool tp(cores_quantity, safe_queue);
-
-	for (size_t i = 1; i > 0 ; --i)
+	Safe_queue<std::function<void()>> safe_queue;
+	
+	for (size_t i = 10; i > 0; --i)
 	{
 		auto lambda = [i] {//this is a task
 			std::cout << i << std::endl;
-			std::this_thread::sleep_for(std::chrono::milliseconds((i % 3) * 100));
+			//std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			};
-		std::function<void()/*int(int)*/> function(lambda);
+		std::function<void()> function(lambda);
 		safe_queue.Submit(std::move(function));
-
 	}
-
-	for (size_t i = 0; i < 1; i++)
+	
+	ThreadPool tp(cores_quantity, std::move(safe_queue));
+	
+	/*for (size_t i = 0; i < 1; i++)
 	{
 		safe_queue.get_result();
-	}
-
+	}*/
+	
 	return 0;
 }
